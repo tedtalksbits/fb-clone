bd44d20bfea0981c071a13b1b0d9bd0f8fc1ec8d
Tedane Blake <tedaneblake@MacBook-Pro.fios-router.home>
2020-10-03 21:38:15 -0400

    Added sidebar and header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBT09TjqV-8PebpCcjbT2fdVWLK_DQ0tOE",
+    authDomain: "facebook-clone-f0991.firebaseapp.com",
+    databaseURL: "https://facebook-clone-f0991.firebaseio.com",
+    projectId: "facebook-clone-f0991",
+    storageBucket: "facebook-clone-f0991.appspot.com",
+    messagingSenderId: "792925296492",
+    appId: "1:792925296492:web:31af0cb6ec99e80fce8549",
+    measurementId: "G-WP2H5N2RXW"
+  };
